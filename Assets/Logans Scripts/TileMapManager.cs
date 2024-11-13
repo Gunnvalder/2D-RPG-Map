fad6c90441e344055285833bf5f91848040e5a71
Gunnvalder <kudlukporter@gmail.com>
2024-11-13 15:29:21 -0400

    worked on the map generation

--- Assets/Logans Scripts/TileMapManager.cs
+++ Assets/Logans Scripts/TileMapManager.cs
@@ -13,7 +13,6 @@ public class TileMapManager : MonoBehaviour
     public Tile chest;
     public Tile door;
 
-    //public int[,] map = new int[20, 20];
     char[,] map = new char[20, 20];
 
 
@@ -25,27 +24,37 @@ public class TileMapManager : MonoBehaviour
 
     void GenerateMap()
     {
+        System.Random randW = new System.Random();
 
         for (int y = 0; y < map.GetLength(1); y++)
         {
             for (int x = 0; x < map.GetLength(0); x++)
             {
+                int randWX = randW.Next(1, map.GetLength(0) - 1);
+                int randWY = randW.Next(1, map.GetLength(1) - 1);
+
                 if (x == 0 || x == map.GetLength(0) - 1 || y == 0 || y == map.GetLength(1) - 1)
                 {
-                    map[x, y] = '#';
+                    
+                    map[randWX, randWY] = '#';//wall
                 }
                 else
                 {
-                    map[x, y] = '.';
+                    map[x, y] = '.';//floor
                 }
             }
         }
+
+        System.Random randD = new System.Random();
 
         for (int y = 1; y < map.GetLength(1) - 1; y++)
         {
             for (int x = 1; x < map.GetLength(1) - 1; x++)
             {
-                if (map[x,y] == '.' && (map[x - 1, y] == '#' || map[x + 1, y] == '#' || map[x, y - 1] == '#' || map[x, y + 1] == '#'))
+                int randDX = randD.Next(1, map.GetLength(0) - 1);
+                int randDY = randD.Next(1, map.GetLength(1) - 1);
+
+                if (map[x, y] == '.' && (map[x - 1, y] == '#' || map[x + 1, y] == '#' || map[x, y - 1] == '#' || map[x, y + 1] == '#'))
                 {
                     map[x, y] = '0'; //door
                 }
@@ -54,15 +63,17 @@ public class TileMapManager : MonoBehaviour
 
         //places chests ($) in random places 
         int chestCount = 0; 
-        System.Random rand = new System.Random();
+        // Spawn random chest (randC)
+        System.Random randC = new System.Random();
         while (chestCount < 2) // max chests
         {
-            int randX = rand.Next(1, map.GetLength(0) - 1);
-            int randY = rand.Next(1, map.GetLength(1) - 1);
+            int randCX = randC.Next(1, map.GetLength(0) - 1);
+            int randCY = randC.Next(1, map.GetLength(1) - 1);
 
-            if (map[randX, randY] == '.' && map[randX - 1, randY] != '#' && map[randX + 1, randY] != '#' && map[randX, randY - 1] != '#' && map[randX, randY + 1] != '#')
+            // place a chest in the map 
+            if (map[randCX, randCY] == '.' && map[randCX - 1, randCY] != '#' && map[randCX + 1, randCY] != '#' && map[randCX, randCY - 1] != '#' && map[randCX, randCY + 1] != '#')
             {
-                map[randX, randY] = '$'; //chest
+                map[randCX, randCY] = '$'; //chest
                 chestCount++;
             }
         }
@@ -76,7 +87,7 @@ public class TileMapManager : MonoBehaviour
             for (int x = 0; x < map.GetLength(0); x++)
             {
                 Vector3Int Cellposition = new Vector3Int(x, y, 0);
-                if (map[x,y] == '#')
+                if (map[x,y] == '#')// wall
                 {
                     MyTilemap.SetTile(Cellposition, wall);
                 }
@@ -84,11 +95,11 @@ public class TileMapManager : MonoBehaviour
                 {
                     MyTilemap.SetTile(Cellposition, floor);
                 }
-                else if (map[x,y] == '0')
+                else if (map[x, y] == '0')// door
                 {
                     MyTilemap.SetTile(Cellposition, door);
                 }
-                else if (map[x,y] == '$')
+                else if (map[x,y] == '$')// chest
                 {
                     MyTilemap.SetTile(Cellposition, chest);
                 }
