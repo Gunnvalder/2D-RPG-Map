6841e54b89a79c32a33eb877d34466c9eb43e92b
Gunnvalder <kudlukporter@gmail.com>
2024-11-12 22:01:53 -0400

    changed the map code for characters

--- Assets/Logans Scripts/TileMapManager.cs
+++ Assets/Logans Scripts/TileMapManager.cs
@@ -8,11 +8,14 @@ public class TileMapManager : MonoBehaviour
 {
 
     public Tilemap MyTilemap;
-    public TileBase wallTile; // Tile for a wall (choose in inspector with a sprite)
-    public TileBase FloorTile;// Tile for a floor (choose in inspector with a sprite)
+    public Tile wall;
+    public Tile floor;
+    public Tile chest;
+    public Tile door;
 
-    public int[,] map = new int[20, 20];
-        
+    //public int[,] map = new int[20, 20];
+    char[,] map = new char[20, 20];
+
 
     private void Start()
     {
@@ -22,19 +25,45 @@ public class TileMapManager : MonoBehaviour
 
     void GenerateMap()
     {
+
         for (int y = 0; y < map.GetLength(1); y++)
         {
             for (int x = 0; x < map.GetLength(0); x++)
             {
                 if (x == 0 || x == map.GetLength(0) - 1 || y == 0 || y == map.GetLength(1) - 1)
                 {
-                    map[x, y] = 1;
+                    map[x, y] = '#';
                 }
                 else
                 {
-                    // random walls and floors
-                    map[x, y] = Random.value < 0.2f ? 1 : 0;
+                    map[x, y] = '.';
                 }
+            }
+        }
+
+        for (int y = 1; y < map.GetLength(1) - 1; y++)
+        {
+            for (int x = 1; x < map.GetLength(1) - 1; x++)
+            {
+                if (map[x,y] == '.' && (map[x - 1, y] == '#' || map[x + 1, y] == '#' || map[x, y - 1] == '#' || map[x, y + 1] == '#'))
+                {
+                    map[x, y] = '0'; //door
+                }
+            }
+        }
+
+        //places chests ($) in random places 
+        int chestCount = 0; 
+        System.Random rand = new System.Random();
+        while (chestCount < 2) // max chests
+        {
+            int randX = rand.Next(1, map.GetLength(0) - 1);
+            int randY = rand.Next(1, map.GetLength(1) - 1);
+
+            if (map[randX, randY] == '.' && map[randX - 1, randY] != '#' && map[randX + 1, randY] != '#' && map[randX, randY - 1] != '#' && map[randX, randY + 1] != '#')
+            {
+                map[randX, randY] = '$'; //chest
+                chestCount++;
             }
         }
 
@@ -47,14 +76,21 @@ public class TileMapManager : MonoBehaviour
             for (int x = 0; x < map.GetLength(0); x++)
             {
                 Vector3Int Cellposition = new Vector3Int(x, y, 0);
-                //wall
-                if (map[x,y] == 1)
+                if (map[x,y] == '#')
                 {
-                    MyTilemap.SetTile(Cellposition, wallTile);
+                    MyTilemap.SetTile(Cellposition, wall);
                 }
-                else // floor 
+                else if (map[x,y] == '.')// floor 
                 {
-                    MyTilemap.SetTile(Cellposition, FloorTile);
+                    MyTilemap.SetTile(Cellposition, floor);
+                }
+                else if (map[x,y] == '0')
+                {
+                    MyTilemap.SetTile(Cellposition, door);
+                }
+                else if (map[x,y] == '$')
+                {
+                    MyTilemap.SetTile(Cellposition, chest);
                 }
             }
         }
